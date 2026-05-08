fb8052ce050ff26ba7b623c8ab0ac43ec4896beb
RIDA FATIMA <ridafatimaa.wahab7819@gmail.com>
2026-05-09 00:40:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+
+//================CHAP 26===========================
+
+// var num = prompt("Enter a num");
+
+// var result = Math.round(num);
+
+// alert("Result  : "+ result);
+
+//==========================//
+
+// var num = prompt("Enter a num");
+
+// var result = Math.floor(num);
+
+// alert(result);
+
+
+//=========================//
+// var num = prompt("Enter a num");
+
+// var result = Math.ceil(num);
+
+// alert(result);  
+
+//================== CHAP 27=============
+
+// var dice1 = Math.floor(Math.random() * 6) + 1;
+// var dice2 = Math.floor(Math.random() * 6) + 1;
+
+// console.log("Random Dice Value 1: " + dice1);
+// console.log("Random Dice Value 2: " + dice2);
+
+//================= CHAP 28=========================
+
+
+// var num = prompt("Enter a  number :");
+// var result = parseInt(num);
+
+// alert(result);
+
+//======================================//
+
+// var num = prompt("Enter a  number :");
+// var result = parseFloat(num);
+
+// alert(result);
+
+//=============== CHAP 29====================
+
+//========convert string into num=========
+// var str = "442";
+// var num = Number(str);
+// alert("Value : " + str);
+// alert("Type : " + typeof str);
+
+// alert("Value : " + num);
+// alert("Type : " + typeof num);
+
+//========convert num into string=========
+// var num = 452;
+
+// var str = num.toString();
+
+// alert("Value: " + str);
+// alert("Type: " + typeof str);
+
+//======================CHAP 30========================
+
+// var num = 5.6789;
+
+// console.log(num.toFixed(2));
+
+//====================================================//
+// var num = 57.39456;
+
+// console.log(num.toFixed(3));
